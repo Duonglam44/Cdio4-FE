274d239e47fc246e8e0621eea41b24c9a5060c87
Duong Lam <duonglam.dev@gmail.com>
2022-04-26 23:46:24 +0700

    share screen, streaming

--- next.config.js
+++ next.config.js
@@ -1,33 +1,34 @@
-const withSass = require('@zeit/next-sass');
-const withFonts = require('nextjs-fonts');
+const withSass = require('@zeit/next-sass')
+const withFonts = require('nextjs-fonts')
 
 function HACK_removeMinimizeOptionFromSassLoaders(config) {
   console.warn(
-    'HACK: Removing `minimize` option from `sass-loader` entries in Webpack config',
-  );
-  config.module.rules.forEach(rule => {
+    'HACK: Removing `minimize` option from `sass-loader` entries in Webpack config'
+  )
+  config.module.rules.forEach((rule) => {
     if (Array.isArray(rule.use)) {
-      rule.use.forEach(u => {
+      rule.use.forEach((u) => {
         if (u.loader === 'sass-loader' && u.options) {
-          delete u.options.minimize;
+          delete u.options.minimize
         }
-      });
+      })
     }
-  });
+  })
 }
 
 module.exports = () => {
-
   const config = {
     COMPANY_ENV: (() => {
-        return withSass(withFonts({
+      return withSass(
+        withFonts({
           webpack(config) {
-            HACK_removeMinimizeOptionFromSassLoaders(config);
-            return config;
+            HACK_removeMinimizeOptionFromSassLoaders(config)
+            return config
           },
-        }));
+        })
+      )
     })(),
-    pageExtensions: ["page.tsx"],
+    pageExtensions: ['page.tsx', 'page.jsx'],
   }
 
   return config
